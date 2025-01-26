406fc34d1ec963a1cce520186bb593c8eade9410
Philippe <145535473+Philippe-Karel@users.noreply.github.com>
2025-01-26 21:39:23 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,11 +7,11 @@ import vragenscript as vs  # Module to generate and check questions
 app = Flask(__name__)
 
 # AI initialisen
-MODEL_PATH = "ai_model/fouten_AI.keras"
+MODEL_PATH = "../ai_model/fouten_AI.keras"
 ai = tf.keras.models.load_model(MODEL_PATH)
 
 # Tokenizer initialisen
-TOKENIZOR_PATH = "ai_model/tokenizer.json"
+TOKENIZOR_PATH = "../ai_model/tokenizer.json"
 ai = tf.keras.models.load_model(MODEL_PATH)
 
 with open(TOKENIZOR_PATH, 'r') as tok:
